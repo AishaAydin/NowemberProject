7d638acc948abd0462b34839d2d7be0919572fe4
AishaAydin <ayseydin13@gmail.com>
2020-11-01 11:24:12 -0500

    Batman has cool bath cycle

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -3,5 +3,7 @@ public class Batman {
         System.out.println("I will catch you Joker");
         System.out.println("Joker laugh");
         //Batman has cool Bat mobile
+        //Batman has cool Bat motorcycle
+
     }
 }
